49ac53bab7d3b5ae4bfcf3b483290cef614b920f
Bipul Islam <bipulhstu@gmail.com>
2024-12-22 12:55:26 +0600

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Bipul Islam on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
